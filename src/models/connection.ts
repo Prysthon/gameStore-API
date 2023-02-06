7fb4dd2656cb0de8cca666409da19101a0a90b99
Tiago <tiagoprysthon14@gmail.com>
2023-02-06 19:41:51 -0300

    fixing: model export connection like constant

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -4,8 +4,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export default mysql.createPool({
-  host: process.env.DB_HOSTNAME || 'localhost',
+const connection = mysql.createPool({
+  host: process.env.DB_HOSTNAME || 'db',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'password',
 });
+
+export default connection;
